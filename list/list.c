f540aaf807dbe7ca2bec18e2d116f4b463d0cd20
maievshabu <fz.uuuu@gmail.com>
2015-10-12 23:35:46 +0800

    list

---
+++ list/list.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+int main()
+{
+	list *l1;
+	init_list(l1);
+	print_list(l1);
+	return 0;
+}
---
+++ list/list.h
@@ -0,0 +1,27 @@
+typedef struct list{
+	int data;
+	int len;
+	struct list* next;
+}list, *plist;
+
+plist init_list(plist list){
+	list->data = 0;
+	list->len = 0;
+	list->next = NULL;
+}
+
+plist push_list(int elem){
+}
+
+void print_list(plist list){
+	plist p;
+	p = list->next;
+	int len = list->len;
+	int i = 0 ;
+	if (len == 0)
+		printf("the is no elem in this list");
+	for(;p != NULL;p++){
+		++i;
+		printf("the current loc is %d and data is %d", i, p->data);
+	}
+}
